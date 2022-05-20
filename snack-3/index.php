b5602f8605e62e0ce2311ad54dcc4cb03f196827
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-05-20 15:22:48 +0200

    change document title

--- snack-3/index.php
+++ snack-3/index.php
@@ -24,7 +24,7 @@ for($i = 0; count($numbers) < 15; $i++) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack-1</title>
+    <title>Snack-3</title>
 </head>
 <body>
     
