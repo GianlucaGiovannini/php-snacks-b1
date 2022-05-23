7deaa1bc263e233fd24b4e1a8d0be944bbfdc003
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-05-23 16:10:37 +0200

    add last snack

--- snack-8/index.php
+++ snack-8/index.php
@@ -5,8 +5,6 @@
 */
 $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam necessitatibus assumenda tenetur voluptas repellat inventore temporibus debitis laudantium, pariatur fugit neque ratione odit recusandae, corporis culpa. Repudiandae aut sit veniam. vel ad laboriosam enim voluptates. Consequuntur magni quod reiciendis maxime non corrupti perspiciatis dolore, sit eos beatae esse dignissimos, eveniet eius nesciunt est nemo at odit. Nemo doloremque vero ratione aut ad natus inventore veniam laboriosam voluptatibus!. Nihil debitis placeat rerum enim, nemo expedita laboriosam tempore laudantium odit consequuntur. Iusto natus quisquam quasi hic perferendis quam delectus totam labore adipisci repellat doloremque, amet beatae necessitatibus, dolores quas optio. Eius repellat aliquam cum fugit expedita quibusdam corporis libero recusandae, deserunt quas nobis magni amet impedit doloribus placeat aperiam dolor maiores provident debitis vero. Recusandae ea quis neque perspiciatis. consectetur illum non nihil laborum ab voluptatum voluptate provident qui assumenda, ex facere quisquam minus numquam cupiditate fuga aperiam, mollitia minima aliquam tempora. Minus deserunt voluptates at suscipit architecto quidem iure quae molestiae quia! Accusantium excepturi, in aperiam corporis soluta laboriosam, tempora assumenda et molestiae sapiente enim rem repellat. Voluptate quia, quos cupiditate molestias natus cum nesciunt ex, repellendus sint numquam veniam cumque explicabo sapiente dolor facere illum culpa distinctio, eligendi ad perspiciatis voluptatum aperiam! Ipsum animi provident vel veniam facere possimus, vero ipsa sit nam mollitia reiciendis laudantium fugit ducimus aperiam excepturi distinctio, perferendis nesciunt exercitationem necessitatibus pariatur fugiat voluptas! Facilis illo et dolorum deleniti. ut modi aliquid rem provident voluptate, mollitia possimus nulla sit officiis vero molestiae beatae animi nostrum, delectus reiciendis magnam. In quis explicabo maxime exercitationem perspiciatis ea illo ducimus esse architecto, nam aperiam animi recusandae magni, id non dolores odit dignissimos excepturi nisi beatae placeat ipsam, molestiae obcaecati. Tenetur, saepe quasi ipsam voluptatum maiores culpa tempora perspiciatis magnam labore quas qui illo iusto mollitia exercitationem. Consequuntur, itaque est.';
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -18,11 +16,14 @@ $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam n
     <title>Snack-8</title>
 </head>
 <body>
-       
+    
+    <h1>Articolo</h1>
+
     <?php foreach(explode('.', $paragraph) as $p) : ?>
         
         <p>
             <?= $p ?>
+            .
         </p>
 
     <?php endforeach ?>
